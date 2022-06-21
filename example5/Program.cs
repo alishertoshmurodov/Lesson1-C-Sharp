c516b118993d6b36e0a8205022c60e63125f18bf
Alisher Toshmurodov <toshmurodov2001@mail.ru>
2022-06-22 00:15:53 +0500

    example 5 Masha

---
+++ example5/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Your name: ");
+string Name = Console.ReadLine();
+if (Name == "Masha"){
+    Console.WriteLine("Welcome, Masha!");
+}
+else {
+    Console.WriteLine("Welcome, user!");
+}
