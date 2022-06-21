8203e5de3167a1677ce2a87bb09a83ffbb185a52
Alisher Toshmurodov <toshmurodov2001@mail.ru>
2022-06-22 00:02:45 +0500

    sum random

--- example3/Program.cs
+++ example3/Program.cs
@@ -1,4 +1,6 @@
-﻿int Number1 = 4;
-int Number2 = 6;
+﻿int Number1 = new Random().Next(1,10);
+Console.WriteLine(Number1);
+int Number2 = new Random().Next(1,10);
+Console.WriteLine(Number2);
 int Sum = Number1 + Number2;
 Console.WriteLine(Sum);
