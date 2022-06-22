a25de5de606b1dd82b633cd7e29d2a654dc3e7b1
Alisher Toshmurodov <toshmurodov2001@mail.ru>
2022-06-22 10:17:39 +0500

    example6

---
+++ example6/Program.cs
@@ -0,0 +1,25 @@
+﻿
+int Num1 = new Random().Next(1,10);
+Console.WriteLine(Num1);
+int Num2 = new Random().Next(1,10);
+Console.WriteLine(Num2);
+int Num3 = new Random().Next(1,10);
+Console.WriteLine(Num3);
+int Num4 = new Random().Next(1,10);
+Console.WriteLine(Num4);
+int max = Num1;
+
+if (max < Num2)
+{
+    max = Num2;
+}
+if (max < Num3)
+{
+    max = Num3;
+}
+if (max < Num4)
+{
+    max = Num4;
+}
+Console.WriteLine("---Max Value---");
+Console.WriteLine(max);
